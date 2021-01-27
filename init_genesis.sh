d453be626828cc9c11c0e7a5d34ea465590f75a4
Pham Thanh Tu <tubackkhoa@gmail.com>
2021-01-27 23:28:24 +0700

    Update init_genesis.sh

--- init_genesis.sh
+++ init_genesis.sh
@@ -1,31 +1,8 @@
 #!/bin/sh
 #set -o errexit -o nounset -o pipefail
 
-pass="$1"
-if [[ ! -z $pass ]] 
-then   
-  # 3 times send passphrase
-  expect << EOF
-    set timeout $timeout
-    spawn $0
-    
-    expect {
-        "*passphrase:" { send -- "$pass\r" }
-    }
-    expect {
-        "*passphrase:" { send -- "$pass\r" }
-    }
-    expect {
-        "*passphrase:" { send -- "$pass\r" }
-    }
-    expect {
-        "*passphrase:" { send -- "$pass\r" }
-    }
-    expect eof
-EOF
-
-  exit 0
-fi 
+echo -n "Enter passphrase:"
+read -s PASSWORD
 
 USER=${USER:-tupt}
 MONIKER=${MONIKER:-node001}
@@ -34,28 +11,19 @@ rm -rf "$PWD"/.oraid
 
 oraid init --chain-id Oraichain "$MONIKER"
 
-if [ ! -z "$pass" ]
-  then 
-    echo "Enter passphrase: "  
-    read pass
-fi
-
-oraid keys add $USER 2>&1 | tee account.txt
+(echo "$PASSWORD"; echo "$PASSWORD") | oraid keys add $USER 2>&1 | tee account.txt
 
 # hardcode the validator account for this instance
-oraid add-genesis-account $USER "100000000000000orai"
+(echo "$PASSWORD") | oraid add-genesis-account $USER "100000000000000orai"
 
 # submit a genesis validator tx
 ## Workraround for https://github.com/cosmos/cosmos-sdk/issues/8251
-oraid gentx $USER "$AMOUNT" --chain-id=Oraichain --amount="$AMOUNT"
+(echo "$PASSWORD"; echo "$PASSWORD") | oraid gentx $USER "$AMOUNT" --chain-id=Oraichain --amount="$AMOUNT" -y
 
 oraid collect-gentxs
 
 oraid validate-genesis
 
-cat $PWD/.oraid/config/genesis.json | jq .app_state.auth.accounts.[0] -c > "$MONIKER"_accounts.txt
-cat $PWD/.oraid/config/genesis.json | jq .app_state.genutil.gen_txs.[0] -c > "$MONIKER"_validators.txt
+cat $PWD/.oraid/config/genesis.json | jq .app_state.genutil.gen_txs[0] -c > "$MONIKER"_validators.txt
 
 echo "The genesis initiation process has finished ..."
-
-
